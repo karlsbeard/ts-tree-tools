36f3fe04e396b71da32c08b1e0c510ae338464c1
karlsbeard <255775675@qq.com>
2024-08-22 22:02:28 +0800

    feat:add the umd & cjs & es6 support

--- src/main.ts
+++ src/main.ts
@@ -72,3 +72,37 @@ export const TTs = {
   },
 
 }
+
+function makeHandlers() {
+  const handlers = {} as { [K in keyof typeof TTs]: typeof TTs[K] }
+  for (const key in TTs) {
+    if (key.startsWith('_'))
+      continue
+    handlers[key] = TTs[key]
+  }
+  return handlers
+}
+
+const handlers = makeHandlers()
+
+const treeHandler = {
+  ...handlers,
+  createInstance(config: Partial<Config> = {}) {
+    const obj = {}
+    for (const key in handlers) {
+      const func = handlers[key]
+      obj[key] = (...args) => func(...args, config)
+    }
+    return obj
+  },
+}
+
+if (typeof window !== 'undefined') {
+  // @ts-expect-error global variable
+  window.TTs = treeHandler
+}
+else if (typeof exports !== 'undefined') {
+  module.exports = treeHandler
+}
+
+export default treeHandler
