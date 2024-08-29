823066f78ab390288d123c3c73241538033584ad
karlsbeard <255775675@qq.com>
2024-08-29 17:56:01 +0800

    fix:refactor the ts-tree-tool

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
+type ToolMethods = {
+  [K in keyof typeof tools]: typeof tools[K] extends (...args: any[]) => any
+    ? (...args: Parameters<typeof tools[K]>) => ReturnType<typeof tools[K]>
+    : never
+}
+
 export interface BaseTreeNode {
   id: string | number
-  children: BaseTreeNode[]
-  [key: string]: any
+  children?: BaseTreeNode[]
 }
 
 export interface BaseListNode {
   id: string | number
   pid: string | number
-  [key: string]: any
 }
 
 interface Config {
@@ -22,9 +26,7 @@ const DEFAULT_CONFIG: Config = {
   children: 'children',
 }
 
-// type ToolsKeys = keyof typeof tools
-
-function getConfig(config: Partial<Config>): Config {
+function getConfig(config = {}): Config {
   return Object.assign({}, DEFAULT_CONFIG, config)
 }
 
@@ -35,21 +37,27 @@ const tools = {
    * @param config the tree config which you want to convert
    * @returns return the tree structure
    */
-  toTree<T extends BaseTreeNode>(list: T[], config: Partial<Config> = {}): T[] {
+  toTree<T extends BaseListNode, K extends BaseTreeNode>(list: T[], config = {}): K[] {
     const { id, pid, children } = getConfig(config)
-    const map = new Map<string | number, T>()
-    const result: T[] = []
+    const nodeMap = new Map<string | number, T>()
+    const result: K[] = []
 
     for (const item of list) {
-      if (!item[children]) {
-        item.children = []
+      if (!item[children as keyof T]) {
+        (item[children as keyof T] as unknown as K[]) = []
       }
-      map.set(item[id], item)
+      nodeMap.set(item[id as keyof T] as string | number, item)
     }
 
     for (const item of list) {
-      const parent = map.get(item[pid])
-       ;(parent ? parent.children : result)?.push(item)
+      const parent = nodeMap.get(item[pid as keyof T] as string | number)
+      const { [pid as keyof T]: _, ...rest } = item
+      if (parent) {
+        (parent[children as keyof T] as unknown as K[]).push(rest as unknown as K)
+      }
+      else {
+        result.push(rest as unknown as K)
+      }
     }
 
     return result
@@ -60,244 +68,244 @@ const tools = {
    * @param config the list config which you want to convert
    * @returns return the list structure
    */
-  toList<T extends BaseTreeNode>(tree: T[], config: Partial<Config> = {}): T[] {
-    const { children } = getConfig(config)
-    const result: T[] = []
+  // toList<T extends BaseTreeNode>(tree: T[], config: Partial<Config> = {}): T[] {
+  //   const { children } = getConfig(config)
+  //   const result: T[] = []
 
-    function loop(list: T[]) {
-      for (const item of list) {
-        const { [children]: _, ...node } = item
-        result.push(node as T)
-        if (item[children]) {
-          loop(item[children])
-        }
-      }
-    }
-    loop(tree)
+  //   function loop(list: T[]) {
+  //     for (const item of list) {
+  //       const { [children]: _, ...node } = item
+  //       result.push(node as T)
+  //       if (item[children]) {
+  //         loop(item[children])
+  //       }
+  //     }
+  //   }
+  //   loop(tree)
 
-    return result
-  },
+  //   return result
+  // },
 
-  /**
-   * @description find the first node by id in the tree
-   * @param tree the tree structure
-   * @param id the id which you want to find
-   * @returns return the node
-   */
-  findNodeByIdBFS<T extends BaseTreeNode>(tree: T[], id: string | number): T | null {
-    if (!tree.length)
-      return null
+  // /**
+  //  * @description find the first node by id in the tree
+  //  * @param tree the tree structure
+  //  * @param id the id which you want to find
+  //  * @returns return the node
+  //  */
+  // findNodeByIdBFS<T extends BaseTreeNode>(tree: T[], id: string | number): T | null {
+  //   if (!tree.length)
+  //     return null
 
-    const queue: T[] = [...tree]
+  //   const queue: T[] = [...tree]
 
-    while (queue.length) {
-      const node = queue.shift()
-      if (node?.id === id) {
-        return node
-      }
+  //   while (queue.length) {
+  //     const node = queue.shift()
+  //     if (node?.id === id) {
+  //       return node
+  //     }
 
-      if (node?.children) {
-        queue.push(...node.children as T[])
-      }
-    }
+  //     if (node?.children) {
+  //       queue.push(...node.children as T[])
+  //     }
+  //   }
 
-    return null
-  },
+  //   return null
+  // },
 
-  findNodeByIdDFS<T extends BaseTreeNode>(tree: T[], id: string | number): T | null {
-    for (const node of tree) {
-      if (node.id === id) {
-        return node
-      }
+  // findNodeByIdDFS<T extends BaseTreeNode>(tree: T[], id: string | number): T | null {
+  //   for (const node of tree) {
+  //     if (node.id === id) {
+  //       return node
+  //     }
 
-      if (node.children) {
-        const children = node.children as T[]
-        const result = tools.findNodeByIdDFS(children, id)
-        if (result) {
-          return result
-        }
-      }
-    }
-    return null
-  },
+  //     if (node.children) {
+  //       const children = node.children as T[]
+  //       const result = tools.findNodeByIdDFS(children, id)
+  //       if (result) {
+  //         return result
+  //       }
+  //     }
+  //   }
+  //   return null
+  // },
 
-  /**
-   * @description find the first node by call function in the tree
-   * @param tree the tree structure
-   */
+  // /**
+  //  * @description find the first node by call function in the tree
+  //  * @param tree the tree structure
+  //  */
 
-  findNodeByFunc<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T | null {
-    for (const node of tree) {
-      if (func(node)) {
-        return node
-      }
-      if (node.children) {
-        const children = node.children as T[]
-        const result = tools.findNodeByFunc(children, func)
-        if (result) {
-          return result
-        }
-      }
-    }
-    return null
-  },
+  // findNodeByFunc<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T | null {
+  //   for (const node of tree) {
+  //     if (func(node)) {
+  //       return node
+  //     }
+  //     if (node.children) {
+  //       const children = node.children as T[]
+  //       const result = tools.findNodeByFunc(children, func)
+  //       if (result) {
+  //         return result
+  //       }
+  //     }
+  //   }
+  //   return null
+  // },
 
-  /**
-   * @description find the all nodes by call function in the tree
-   * @param tree the tree structure
-   */
+  // /**
+  //  * @description find the all nodes by call function in the tree
+  //  * @param tree the tree structure
+  //  */
 
-  findAllNode<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[] {
-    const result: T[] = []
-    for (const node of tree) {
-      if (func(node)) {
-        result.push(node)
-      }
-      if (node.children) {
-        const children = node.children as T[]
-        result.push(...tools.findAllNode(children, func))
-      }
-    }
-    return result
-  },
+  // findAllNode<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[] {
+  //   const result: T[] = []
+  //   for (const node of tree) {
+  //     if (func(node)) {
+  //       result.push(node)
+  //     }
+  //     if (node.children) {
+  //       const children = node.children as T[]
+  //       result.push(...tools.findAllNode(children, func))
+  //     }
+  //   }
+  //   return result
+  // },
 
-  /**
-   * @description find the first path by id in the tree
-   * @param tree the tree structure
-   * @param id the id which you want to find
-   * @returns return the path
-   */
+  // /**
+  //  * @description find the first path by id in the tree
+  //  * @param tree the tree structure
+  //  * @param id the id which you want to find
+  //  * @returns return the path
+  //  */
 
-  findPathByIdBFS<T extends BaseTreeNode>(tree: T[], id: string | number): T[] | null {
-    if (!tree.length)
-      return null
+  // findPathByIdBFS<T extends BaseTreeNode>(tree: T[], id: string | number): T[] | null {
+  //   if (!tree.length)
+  //     return null
 
-    const queue: { node: T, path: T[] }[] = []
+  //   const queue: { node: T, path: T[] }[] = []
 
-    for (const node of tree) {
-      queue.push({ node, path: [node] })
-    }
+  //   for (const node of tree) {
+  //     queue.push({ node, path: [node] })
+  //   }
 
-    while (queue.length) {
-      const { node, path } = queue.shift()!
-      if (node.id === id) {
-        return path
-      }
+  //   while (queue.length) {
+  //     const { node, path } = queue.shift()!
+  //     if (node.id === id) {
+  //       return path
+  //     }
 
-      if (node.children) {
-        const children = node.children as T[]
-        for (const child of children) {
-          queue.push({ node: child, path: [...path, child] })
-        }
-      }
-    }
+  //     if (node.children) {
+  //       const children = node.children as T[]
+  //       for (const child of children) {
+  //         queue.push({ node: child, path: [...path, child] })
+  //       }
+  //     }
+  //   }
 
-    return null
-  },
+  //   return null
+  // },
 
-  findPathByIdDFS<T extends BaseTreeNode>(tree: T[], id: string | number): T[] | null {
-    const path: T[] = []
+  // findPathByIdDFS<T extends BaseTreeNode>(tree: T[], id: string | number): T[] | null {
+  //   const path: T[] = []
 
-    function dfs(nodes: T[]): boolean {
-      for (const node of nodes) {
-        path.push(node)
-        if (node.id === id) {
-          return true
-        }
-        if (node.children) {
-          const children = node.children as T[]
-          if (dfs(children)) {
-            return true
-          }
-        }
-        path.pop()
-      }
-      return false
-    }
+  //   function dfs(nodes: T[]): boolean {
+  //     for (const node of nodes) {
+  //       path.push(node)
+  //       if (node.id === id) {
+  //         return true
+  //       }
+  //       if (node.children) {
+  //         const children = node.children as T[]
+  //         if (dfs(children)) {
+  //           return true
+  //         }
+  //       }
+  //       path.pop()
+  //     }
+  //     return false
+  //   }
 
-    return dfs(tree) ? path : null
-  },
+  //   return dfs(tree) ? path : null
+  // },
 
-  /**
-   * @description find the first path by func in the tree within BFS
-   * @param tree the tree structure
-   * @param func the function which you want to find
-   * @returns return the path, maybe the null
-   */
+  // /**
+  //  * @description find the first path by func in the tree within BFS
+  //  * @param tree the tree structure
+  //  * @param func the function which you want to find
+  //  * @returns return the path, maybe the null
+  //  */
 
-  findPathByFuncBFS<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[] | null {
-    const path: T[] = []
-    const list: T[] = [...tree]
-    const visitedSet = new Set<T>()
+  // findPathByFuncBFS<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[] | null {
+  //   const path: T[] = []
+  //   const list: T[] = [...tree]
+  //   const visitedSet = new Set<T>()
 
-    while (list.length) {
-      const node = list[0]
-      if (visitedSet.has(node)) {
-        path.pop()
-        list.shift()
-      }
-      else {
-        visitedSet.add(node)
-        const children = node.children as T[] | undefined
-        if (children) {
-          list.unshift(...children)
-        }
-        path.push(node)
-        if (func(node)) {
-          return path
-        }
-      }
-    }
-    return null
-  },
+  //   while (list.length) {
+  //     const node = list[0]
+  //     if (visitedSet.has(node)) {
+  //       path.pop()
+  //       list.shift()
+  //     }
+  //     else {
+  //       visitedSet.add(node)
+  //       const children = node.children as T[] | undefined
+  //       if (children) {
+  //         list.unshift(...children)
+  //       }
+  //       path.push(node)
+  //       if (func(node)) {
+  //         return path
+  //       }
+  //     }
+  //   }
+  //   return null
+  // },
 
-  /**
-   * @description find the first path by func in the tree within DFS
-   * @param tree the tree structure
-   * @param func the function which you want to find
-   * @returns return the path
-   */
+  // /**
+  //  * @description find the first path by func in the tree within DFS
+  //  * @param tree the tree structure
+  //  * @param func the function which you want to find
+  //  * @returns return the path
+  //  */
 
-  findPathByFuncDFS<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[] | null {
-    const path: T[] = []
-    for (const node of tree) {
-      if (func(node)) {
-        return path
-      }
-      if (node.children) {
-        const children = node.children as T[]
-        const path = tools.findPathByFuncDFS(children, func)
-        if (path) {
-          return path
-        }
-      }
-    }
-    return null
-  },
+  // findPathByFuncDFS<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[] | null {
+  //   const path: T[] = []
+  //   for (const node of tree) {
+  //     if (func(node)) {
+  //       return path
+  //     }
+  //     if (node.children) {
+  //       const children = node.children as T[]
+  //       const path = tools.findPathByFuncDFS(children, func)
+  //       if (path) {
+  //         return path
+  //       }
+  //     }
+  //   }
+  //   return null
+  // },
 
-  /**
-   * @description find all path by func in the tree
-   * @param tree  the tree structure
-   * @param func the function which you want to find
-   * @returns all path which you want to find
-   */
+  // /**
+  //  * @description find all path by func in the tree
+  //  * @param tree  the tree structure
+  //  * @param func the function which you want to find
+  //  * @returns all path which you want to find
+  //  */
 
-  findPathAll<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[][] {
-    const allPath: T[][] = []
-    for (const node of tree) {
-      if (func(node)) {
-        allPath.push([node])
-      }
-      if (node.children) {
-        const children = node.children as T[]
-        const childPaths = tools.findPathAll(children, func)
-        for (const childPath of childPaths) {
-          allPath.push([node, ...childPath])
-        }
-      }
-    }
-    return allPath
-  },
+  // findPathAll<T extends BaseTreeNode>(tree: T[], func: (node: T) => boolean): T[][] {
+  //   const allPath: T[][] = []
+  //   for (const node of tree) {
+  //     if (func(node)) {
+  //       allPath.push([node])
+  //     }
+  //     if (node.children) {
+  //       const children = node.children as T[]
+  //       const childPaths = tools.findPathAll(children, func)
+  //       for (const childPath of childPaths) {
+  //         allPath.push([node, ...childPath])
+  //       }
+  //     }
+  //   }
+  //   return allPath
+  // },
 
   /**
    * @description filter the tree by func
@@ -320,14 +328,35 @@ const tools = {
 }
 
 // to get all handlers
-// function makeHandlers<T extends ToolsKeys>(keys: T[]): Record<T, typeof tools[T]> {
-//   const obj: Record<string, any> = {}
+function makeHandlers(): ToolMethods {
+  const obj = {} as ToolMethods
+  for (const key in tools) {
+    if (key.startsWith('_'))
+      continue
+    const toolKey = key as keyof typeof tools
+    obj[toolKey] = tools[toolKey] as typeof tools[typeof toolKey]
+  }
+  return obj
+}
 
-//   for (const key of keys) {
-//     obj[key as string] = tools[key]
-//   }
+const handlers = makeHandlers()
 
-//   return obj as Record<T, typeof tools[T]>
-// }
-
-// const handlers = makeHandlers(Object.keys(tools))
+export const TTs = {
+  ...handlers,
+  createInstance(config = {}) {
+    const obj = {} as ToolMethods
+    for (const key in handlers) {
+      const func = handlers[key as keyof ToolMethods]
+      obj[key as keyof ToolMethods] = (...args: Parameters<typeof func>) => {
+        if (args.length > 0) {
+          args[args.length - 1] = config
+        }
+        else {
+          args.push(config)
+        }
+        return func(...args)
+      }
+    }
+    return obj
+  },
+}
